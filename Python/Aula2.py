b87d34a383cb2e1def075a16bb91d61d550ae4da
Ivan <ivansantana.sax@gmail.com>
2021-07-03 10:45:18 -0300

    Aula 2, com format e input do usuário

---
+++ Python/Aula2.py
@@ -0,0 +1,12 @@
+a = int(input('Qual o valor para a? '))
+b = int(input('Qual o valor de b? '))
+soma = a + b
+subtracao = a - b
+multi = a * b
+divi = a / b
+print ('A SOMA: entre ', a, ' e ', b, ' é igual a ',soma)
+print ('A DIFERENÇA entre ', a, ' e ', b, ' é igual a ',subtracao)
+print ('O PRODUTO entre ', a, ' e ', b, ' é igual a ',multi)
+print ('O QUOCIENTE entre ', a, ' e ', b, ' é igual a ',divi)
+
+print("Soma: {} \nDiferença: {} \nProduto: {} \nQuociente: {}".format(soma, subtracao,multi, divi))
---
+++ Python/Meu_app.py
@@ -0,0 +1,5 @@
+print ('Meu primeiro programa em PYTHON!')
+a = 2
+b = 3
+soma = a + b
+print ('A soma de ', a, ' e ', b, 'é igual a ', soma)
